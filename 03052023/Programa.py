4d8eb6c10be36cbdb03a3f35060b8f8a607046f0
Diego Carranza <diego300carranza754@gmail.com>
2023-05-03 17:33:38 -0500

    listas

---
+++ 03052023/Programa.py
@@ -0,0 +1,22 @@
+import random
+suma=0
+cont=0
+mayor=0
+menor=0
+lista=[]
+tam=random.randint(10,20)
+
+for a in range(tam):
+    b=random.randrange(100)
+    lista.append(b)
+print(lista)
+for c in lista:
+    suma+=c
+    cont+=1
+print(f'las suma es {suma}')
+for d in lista:
+     if d>mayor:
+         mayor=d
+     if d<menor:
+         menor=d
+print(f'El numero mayor es {mayor}')
