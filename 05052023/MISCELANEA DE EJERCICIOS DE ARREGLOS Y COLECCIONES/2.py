b8e281a52a8da24dcaec717db4eb2d3f759f3192
Diego Carranza <diego300carranza754@gmail.com>
2023-05-08 07:06:09 -0500

    Repositorio13

--- 05052023/MISCELANEA DE EJERCICIOS DE ARREGLOS Y COLECCIONES/2.py
+++ 05052023/MISCELANEA DE EJERCICIOS DE ARREGLOS Y COLECCIONES/2.py
@@ -5,6 +5,8 @@ final1=int(input('ingrese un rango final: '))
 suma1=0
 suma2=0
 contador=0
+par=0
+impar=0
 tam=random.randint(inicio1,final1)
 promedioconj=0
 for b in range(tam):
@@ -29,9 +31,21 @@ for c in lista2:
 print(f'La suma de los numeros de la lista1 es {suma1}')
 print(f'La suma de los numeros de la lista2 es {suma2}')
 promedio2=suma2/tam
-# print(f'el promedio de la lista2 de numeros es {promedio2}')
-# print(f'el promedio de la lista1 de numeros es {promedio1}')
 if suma1>suma2:
     print(f'la suma de la lista1 es mayor que la de la lista2')
 else: print(f'la suma de la lista2 es mayor que la de la lista1')
 Lista=[lista1+lista2]
+print(f'la lista en conjunto es: \n {Lista}')
+promedioconj=(promedio1+promedio2)/2
+print(f'el promedio en conjunto es {promedioconj}')
+print(f'el promedio de la lista1 de numeros es {promedio1}')
+if promedio1>promedioconj:
+    print(f'el promedio de la lista1 es mayor que el el promedio en conjunto')
+print(f'el promedio de la lista2 de numeros es {promedio2}')
+if promedio2>promedioconj:
+    print(f'el promedio de la lista2 es mayor que el el promedio en conjunto')
+if promedioconj>promedio1 or promedioconj>promedio2:
+    print(f'el promedio en conjunto sigue siento mayor')
+for d in lista1:
+    if par!=0:
+        print(d)
