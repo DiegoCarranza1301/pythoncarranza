a8d2bf28fe37447c73612eaa0231f6436488f16b
Diego Carranza <diego300carranza754@gmail.com>
2023-06-06 15:22:19 -0500

    Repositorio22

--- 05062023/salarios.py
+++ 05062023/salarios.py
@@ -1,8 +1,8 @@
 class Empleado:
-    def __init__(self,nombre,cargo):
+    def __init__(self,nombre,cargo,salario):
         self.__nombre=nombre
         self.__cargo=cargo
-        self.__salario=int()
+        self.__salario=salario
     def Nom(self, nombre):
         self.__nombre=nombre
     def Carg(self,cargo):
@@ -14,8 +14,8 @@ class Empleado:
     def aggCarg(self):
         return self.__cargo
     def aggSal(self):
-        return self.__salario
-e1=Empleado('Diego', 'Inspector')
+        return self.__salario   
+e1=Empleado('Diego', 'Jefe', 40000000)
 print(e1.aggNom())
 print(e1.aggCarg())
-print(e1.sal)
+print(e1.aggSal())
